ec29cabc271027cacb5a2d6b73797d6b92c33887
timmmerlan <bunnytim7@gmail.com>
2020-01-08 10:52:27 +0100

    initial commit

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget -O "buildings_way.geojson" "https://overpass-api.de/api/interpreter?data=way[building](37.451967,14.658508,38.021050,15.298462);(._;>;);out;"
+wget -O "buildings_rel.geojson" "https://overpass-api.de/api/interpreter?data=rel[building](37.451967,14.658508,38.021050,15.298462);(._;>;);out;"
