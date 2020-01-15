7205bdfc54aca48c4d7d7359b58e2f7acb91048f
Tim <timm.merlan@web.de>
2020-01-15 21:04:21 +0100

    grass gis script for import of osm data and reclassification of street density

--- download.sh
+++ download.sh
@@ -1,2 +1,5 @@
 wget -O "buildings_way.geojson" "https://overpass-api.de/api/interpreter?data=way[building](37.451967,14.658508,38.021050,15.298462);(._;>;);out;"
 wget -O "buildings_rel.geojson" "https://overpass-api.de/api/interpreter?data=rel[building](37.451967,14.658508,38.021050,15.298462);(._;>;);out;"
+#wget -O "streets.geojson" "https://overpass-api.de/api/interpreter?data=way[highway](37.451967,14.658508,38.021050,15.298462);(._;>;);out;"
+
+#python ./etna/vulnerability.py
